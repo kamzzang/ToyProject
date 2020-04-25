51261b7e4dc43d29cdf93f776dd6f419d1169d2b
Moochul Kam <kamzzang1@naver.com>
2020-04-25 23:09:33 +0900

    First commit

--- KakaoChatbot/app_chatbot.py
+++ KakaoChatbot/app_chatbot.py
@@ -1,45 +1,194 @@
+# flask용 모듈
 from flask import Flask, request, jsonify
+# crawling용 모듈
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import requests
 import urllib
 
-ERROR_MESSAGE = 'Network Connecting Error'
-
 app = Flask(__name__)
 
-@app.route('/weather', methods=['POST'])
+# 봇에 요청이 올 때마다 request로 요청하면 응답 속도가 느려지는 문제가 확인되어 봇 실행 시 필요한  request 및 beautifulsoup 완료 후 대기
+# 1. 영화 예매 순위 응답용
+url = 'https://movie.naver.com/movie/running/current.nhn' # 네이버 영화 웹 페이지 - 현재 상영영화 - 예매순
+response = requests.get(url)
+soup_rank = BeautifulSoup(response.text, 'html.parser')
+# 2. 개봉 예정 영화 순위 응답용
+url = 'https://movie.naver.com/movie/running/premovie.nhn?order=reserve' # 네이버 영화 웹 페이지 - 개봉 예정 영화 - 예매순
+response = requests.get(url)
+soup_schdule = BeautifulSoup(response.text, 'html.parser')
+
+def movie_search(search_type, start_cnt): # 영화 정보 제공 서비스 실행용 함수
+    
+    img_url = []        # 포스터 경로 url
+    title = []          # 영화 제목
+    description = []    # 세부 정보 : 영화 예매 순위 응답 - 평점과 예매율, 개봉 예정작 응답 - 개봉예정일
+    link_url = []       # 영화 예매 및 정보가 제공되는 사이트로 연결을 위한 웹 페이지 경로 url
+    
+    if search_type == 'rank': # 영화 예매 순위 요청
+        soup = soup_rank
+        
+        img_tag = soup.find_all("div", {"class":"thumb"})   # 영화 포스터 이미지, 제목, 정보제공 링크가 있는 태그
+        cnt = 1
+        for src in img_tag:
+            if cnt >= start_cnt and cnt < start_cnt+5:      # 카트 리스트 응답은 한번에 최대 5개만 가능하므로 정보를 5개만 저장함
+                src_img = src.find('img')
+                img_url.append(src_img.get('src'))
+                title.append(src_img.get('alt'))
+
+                src_link = src.find('a')
+                link_url.append('https://movie.naver.com' + src_link.get('href')) # 링크 url 완성
+            cnt+=1
+        img_url.insert(0,'')            # 카트 리스트로 응답을 보내기 위해서 위해 첫 인덱스에는 제목같은 내용이 들어가므로 각 데이터에 내용 삽입
+        title.insert(0,'영화 예매 순위') # 카트 리스트 제목
+        link_url.insert(0,url)          # 카트 리스트 제목란은 크롤링 페이지로 이동가능하도록 링크 삽입
+
+        get_score = soup.find_all("span",{"class" : "num"}) # 평점과 예매율이 있는 태크
+        cnt=1
+        temp=''
+        for i in get_score:
+            if start_cnt == 1:
+                if cnt >= 1 and cnt < 11: # 평점, 예매율이 번갈아 가면서 저장되기 때문에 5개의 영화에 대해서 총 10개의 데이터를 받음
+                    if cnt % 2 == 1: 
+                        temp = i.text
+                    else:
+                        description.append('평점 : ' + temp + '\t' + '예매율 : ' + i.text + '%') # 세부 정보에 평점과 예매율 저장
+            else:
+                if cnt >= 11 and cnt < 21: 
+                    if cnt % 2 == 1: 
+                        temp = i.text
+                    else:
+                        description.append('평점 : ' + temp + '\t' + '예매율 : ' + i.text + '%')
+            cnt+=1
+        description.insert(0,'')
+        
+        button_message = "영화 예매 순위 더보기" # 총 10위까지 응답을 위해서 첫 메시지에는 "순위 더보기 버튼"을 넣어주기 위한 버튼 클릭 시 발화되는 메세지
+        
+    else:
+        soup = soup_schdule
+        
+        img_tag = soup.find_all("div", {"class":"thumb"})
+        cnt = 1
+        for src in img_tag:
+            if cnt >= start_cnt and cnt < start_cnt+5:
+                src_img = src.find('img')
+                img_url.append(src_img.get('src'))
+                title.append(src_img.get('alt'))
+
+                src_link = src.find('a')
+                link_url.append('https://movie.naver.com' + src_link.get('href'))
+            cnt+=1
+        img_url.insert(0,'')
+        title.insert(0,'개봉예정 영화')
+        link_url.insert(0,url)
+        
+        sch_date = soup.find_all("dl", {'class' : 'info_txt1'})
+        cnt=1
+        for i in sch_date:
+            if cnt >= start_cnt and cnt < start_cnt+5:
+                temp = i.text.replace('\t','').replace('\n','').replace('\r','').split(',')
+                for text in temp:
+                    if '개봉' in text: temp=text
+                temp = temp.split('|')[-1].split('감독')[0]
+                description.append(temp)
+            cnt+=1
+        description.insert(0,'')
+        
+        button_message = "개봉 예정 영화 더보기"
+
+    # 응답 메시지가 첫 메시지인지 더보기 요청인지에 따라 첫 메시지일 때만 "더보기" 버튼 생성, 순위 표시    
+    if start_cnt == 1: # 첫번째 응답 메시지에 순위와 출처 추가 및 버튼 생성
+        title[0] = title[0] + '(1위~5위)_출처: Naver영화' 
+        button_data = [
+                        {
+                          "type": "block",
+                          "label": "+ 더보기",
+                          "message" : button_message, # 버튼 클릭 시 사용자가 전송한 것과 동일하게 하는 메시지
+                          "data": {
+                            }
+                        }
+                      ]
+    else:
+        title[0] = title[0] + '(6위~10위)_출처: Naver영화'
+        button_data = [
+                {
+                  "type": "text", # 버튼 타입을 텍스트로 하고 라벨 및 메시지를 비우면 버튼이 나오지 않음(두 번의 메시지를 동일한 포맷으로 res 변수로 만들기 위함)
+                  "label": "",
+                  "message" : "",
+                  "data": {
+                    }
+                }
+              ]
+        
+        
+    listItems=[]
+
+    cnt=0
+    for i in range(6): # 응답용 카트 리스트 타입의 res에 추가할 정보 완성
+        if cnt == 0: itemtype = 'title' # 카드 이미지의 첫 type은 title
+        else: itemtype = 'item'         # 카드 이미지의 제목 다음 type은 item
+            
+        listItems.append({
+                "type": itemtype,               # 카드 리스트의 아이템 티입
+                "imageUrl": img_url[i],         # 이미지 링크 url
+                "title": title[i],              # 제목
+                "description": description[i],  # 세부 정보
+                "linkUrl": {
+                  "type": "OS",                 # PC나 모바일별 별도 url설정 가능하나 web용으로 동일 적용
+                    "webUrl": link_url[i]       # 영화 정보 링크 url
+                    }
+                })
+        cnt+=1
+        
+    return listItems, button_data
+
+    
+@app.route('/movies', methods=['POST']) # 영화 정보 블럭에 스킬로 연결된 경로
+def movies():
+    req = request.get_json()
+    
+    input_text = req['userRequest']['utterance'] # 사용자가 전송한 실제 메시지
+    
+    if '개봉' in input_text: # 전송 메시지에 "개봉"이 있을 경우는 개봉 예정작 정보를 응답
+        search_type = 'schdule'
+    else:                   # "개봉"이 메시지에 없으면 예매 순위를 응답
+        search_type = 'rank'
+        
+    if '더보기' in input_text: # 더보기를 요청했을 경우는 메시지에 더보기가 입력되게 설정을 해서 이 경우는 6위부터 10위까지 저장
+        start_cnt = 6
+    else:
+        start_cnt = 1         # 첫 요청일 경우 1위 부터 5위까지 저장
+
+    # 검색 타입(예매 순위 or 개봉 예정작)과 검색 시작 번호를 movie_search 함수로 전달하여 아이템과 버튼 설정을 반환받음  
+    listItems, button_data = movie_search(search_type, start_cnt) 
+
+    # 카드 리스트형 응답용 메시지
+    res = {
+          "contents": [
+            {
+              "type": "card.list",
+              "cards": [
+                {
+                  "listItems": listItems,
+                    "buttons": button_data
+                }
+              ]
+            }
+          ]
+        }            
+
+    # 전송
+    return jsonify(res)
+
+
+@app.route('/weather', methods=['POST']) # 날씨 정보 블럭에 스킬로 연결된 경로
 def weather():
 
     req = request.get_json()
 
-    # location = req["action"]["detailParams"]["sys_location"]["value"]
-    # print(location)
-    # enc_loc = urllib.parse.quote(location + '+ 날씨')
-    # el = str(enc_loc)
-    # url = 'https://search.naver.com/search.naver'
-    # url = url + '?sm=top_hty&fbm=1&ie=utf8&query='
-    # url = url + el
-
-    # req = Request(url)
-    # page = urlopen(req)
-    # html = page.read()
-    # soup = BeautifulSoup(html, 'html.parser')
-    # r1 = soup.find('li', class_='on now merge1')
-    # r2 = r1.find('dd', class_='weather_item _dotWrapper')
-    # r3 = r2.find('span').text
-
-    # rain_pct = int(r3)
-
-    # if len(location) <= 0:
-    #     answer = ERROR_MESSAGE
-    # elif rain_pct < 30:
-    #     answer = location + "의 강수 확률은 " + r3 + "%입니다 맑은 하루 되세요^_^"
-    # else:
-    #     answer = location + "의 강수 확률은 " + r3 + "%입니다 우산 챙겨가세요!!"
+    answer = '날씨 정보 제공 서비스 준비중입니다.' # 날씨 정보는 추후 작업 예정
     
-    answer = '날씨 정보 제공 서비스'
-    
+    # 일반 텍스트형 응답용 메시지
     res = {
         "version": "2.0",
         "template": {
@@ -55,38 +204,6 @@ def weather():
 
     return jsonify(res)
 
-@app.route('/movies', methods=['POST'])
-def movies():
-    req = request.get_json()
-    # print(req)
-    
-    # 1. 작성 예시에서의 버튼으로 진입인지 메시지 입력 전송으로 진입인지 확인
-    #    버튼은 예매 순위와 개봉 영화 정보만 제공함
-    # 2. 
-    path = req['intent']['extra']['reason']['message'] 
-    if path == 'DIRECT_ID':
-        if '개봉' in req['userRequest']['utterance']:
-            answer = '입력 예시 창 버튼 입력_개봉예정영화'
-        else:
-            answer = '입력 예시 창 버튼 입력_예매율 순위'
-        
-    elif path == 'OK':
-        answer = '사용자 전송 입력'
-        
-    res = {
-        "version": "2.0",
-        "template": {
-            "outputs": [
-                {
-                    "simpleText": {
-                        "text": answer
-                    }
-                }
-            ]
-        }
-    }
-
-    return jsonify(res)
 
 # 메인 함수
 if __name__ == '__main__':
